cdbb63149317f513dd453603c95764056ecbd2b4
rokon1127 <rokon905@gmail.com>
2023-06-07 12:56:09 -0500

    Adding ending code

---
+++ Calculator.py
@@ -0,0 +1,23 @@
+print("=================Simple Calculator====================================")
+
+
+first_number =int(input("Enter Your Number: "))
+second_number =int(input("Enter Another Number: "))
+operation =input("Enter Operation: ")
+
+user_input01 = first_number
+
+user_input02 = second_number
+op = operation
+
+if op=="+":
+    print("Add: ", user_input01+user_input02 )
+
+elif op=="-":
+    print("SUB: ", user_input01-user_input02 )
+
+elif op=="*":
+    print("Mul: ", user_input01*user_input02 )
+
+elif op=="/":
+    print("Div: ", user_input01/user_input02 )
---
+++ Calendar.py
@@ -0,0 +1,21 @@
+import calendar
+
+year = int(input("Enter Calender year: "))
+month = int(input("Enter Calender Month: "))
+
+print("You Entered: ",  year)
+print("You Entered: ", month)
+
+yy = year #year
+mm = month #month
+#display the calender
+print(calendar.month(yy, mm))
+
+#todays date and time
+import datetime
+now =datetime.datetime.now()
+print(now)
+
+
+
+
