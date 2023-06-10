4fec1802a61cb7d056c0397bc5b65bebbddcf214
rokon1127 <rokon905@gmail.com>
2023-06-10 18:47:56 -0500

    age calculator

--- AgeCalculator.py
+++ AgeCalculator.py
@@ -1,1 +1,13 @@
-print("Lets Check Your Age")
+print("==================AgeCalculator======================+")
+
+from datetime import date
+year = int(input("Birth Year: "))
+month = int(input("Birth month: "))
+day = int(input("Birth Day: "))
+
+today = date.today()
+birthdate = date(year, month, day)
+age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+
+print("Your age is: " , age, "years")
+
